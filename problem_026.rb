b595a7eb9872d33445e874b4ff3468bb18459df4
Pedro B. Coronel <pbcoronel@pbcoronel-xubuntu.(none)>
2013-04-26 10:00:08 -0400

    new problems

---
+++ problem_026.rb
@@ -0,0 +1,33 @@
+puts "Enter limit"
+
+limit = gets.chomp.to_i
+# Just doing some long division...
+
+
+maxcycle = 0
+maxd = 0
+
+for i in 2...limit
+	num = 1
+	nums = []
+	cycle = 0
+	loop do 
+		divnum = num * 10
+		if nums.include?(divnum)
+			divnum == 0 ? cycle = 0 : cycle = nums.length - nums.index(divnum)
+			break
+		end
+		nums << divnum
+		division = divnum / i
+		division = division.to_i
+		num = divnum - (division * i)
+	end
+	puts "#{i}, #{cycle}"
+	maxd = i if cycle > maxcycle
+	maxcycle = cycle if cycle > maxcycle
+end
+
+
+puts ""
+puts "Max d: #{maxd}"
+puts "Max cycle: #{maxcycle}"
---
+++ problem_027.rb
@@ -0,0 +1,7 @@
+require 'prime'
+
+n = 40
+
+for a in -999..999
+	puts a
+end
