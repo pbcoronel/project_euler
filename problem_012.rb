a27474d5a6f580d58503a8f370a10ecf7dac6591
Pedro B. Coronel <pedro.coronel@forislabs.com>
2013-05-16 16:42:52 -0400

    changes

--- problem_012.rb
+++ problem_012.rb
@@ -1,4 +1,7 @@
 # Using divisor number function based on the exponents of the prime divisors
+# if n = (p**a)*(q**b)*(r**c).. where p,q,r,.. are the prime factors of n
+# then the number of divisors of n d(n)=(a+1)*(b+1)*(c+1)*...
+
 require 'prime'
 
 puts "How many divisors?"
