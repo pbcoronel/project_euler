3b5b8f73dab046fa9ff924a651418492213c9d26
Pedro B. Coronel <pedro.coronel@forislabs.com>
2013-05-28 16:44:19 -0400

    refactoring

--- problem_006.rb
+++ problem_006.rb
@@ -1,6 +1,5 @@
 squareofsum = (100*101/2) ** 2
 sumofsquares = 0
-for i in 1..100
-	sumofsquares += i**2
-end
+(1..100).each {|i| sumofsquares += i**2}
+
 puts squareofsum - sumofsquares
--- problem_007.rb
+++ problem_007.rb
@@ -1,36 +1,16 @@
-def is_prime?(x)
-	is_prime = true
-	digits = x.to_s.split("").map {|digit| digit.to_i}
-	#Multiples rules check
-	if x > 2 && digits.last % 2 == 0
-		is_prime = false
-	elsif x > 5 && (digits.last == 0 || digits.last == 5)
-		is_prime = false
-	# Brute force check
-	else
-		for i in 2..x-1
-			if x % i == 0
-				is_prime = false
-			end
-	end	end
-	is_prime
-end
+require "prime"
 
 puts "How many primes do you want?"
-num = gets.chomp
-num = Integer(num)
+num = gets.chomp.to_i
 
-found = 0
-
-i = 2
+found, i = 0, 1
 
 until found == num
-	if is_prime?(i)
-		found += 1
-		puts found.to_s + ", " + i.to_s
-	end
-	i +=1
+  i +=1
+  found += 1 if i.prime?
 end
+
+puts "Prime number #{num}: #{i}"
 
 
 
