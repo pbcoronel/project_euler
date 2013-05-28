250b402c7fbb29774fb6bb1e97fb1fab45ed3f1c
Pedro B. Coronel <pedro.coronel@forislabs.com>
2013-05-28 15:18:37 -0400

    refactoring

--- problem_003.rb
+++ problem_003.rb
@@ -1,42 +1,26 @@
-# ad-hoc prime checker. using the library is, of course, much more efficient
+# Ad-hoc prime checker. Using the library is, of course, much more efficient
 # but this works ok for now.
 
 def is_prime?(x)
-  is_prime = true
   digits = x.to_s.split("").map {|digit| digit.to_i}
   #Multiples rules check
-  if x > 2 && digits.last % 2 == 0
-	is_prime = false
-  elsif x > 3 && digits.inject(:+) % 3 == 0
-  	is_prime = false
-  elsif x > 5 && (digits.last == 0 || digits.last == 5)
-	is_prime = false
-  # Brute force check
-  else
-	for i in 2..x-1
-   	  if x % i == 0
-		is_prime = false
-	  end
-	end	
-  end
-  is_prime
+  return false if x > 2 && digits.last % 2 == 0
+  return false if x > 3 && digits.inject(:+) % 3 == 0
+  return false if x > 5 && (digits.last == 0 || digits.last == 5)
+  return false if (2...x).select{|n| x%n==0}.length != 0  
+  true
 end
 
 puts "Which number do you want the prime factors for?"
-num = gets.chomp
-num = Integer(num)
+num = gets.chomp.to_i
+factors = []
 
-
-start = Time.now
-
-primefactor = 1
-
-for i in 2..num
-	if num % i == 0 && is_prime?(i)
-		puts i
-	end
+while num > 1 do
+  pfactor = (2..num).find {|n| num % n == 0}
+  factors << pfactor
+  num = num / pfactor
 end
 
-system("./script1", "argX")
-puts "Duration: #{Time.now - start} seconds"
-
+puts "The unique prime factors are:"
+p factors.uniq
+puts "The largest prime factor is #{factors.max}"
