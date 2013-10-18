5d15e178c63adb0948b66bdeac01796c2c6ff708
Pedro B. Coronel <pedro.b.coronel@gmail.com>
2013-10-17 21:36:23 -0500

    New problem

--- java/src/problem_001.java
+++ java/src/problem_001.java
@@ -1,5 +1,24 @@
-class hello {
+/**
+ * Created with IntelliJ IDEA.
+ * User: pbcoronel
+ * Date: 10/17/13
+ * Time: 9:16 PM
+ */
+public class problem_001 {
     public static void main(String[] args) {
-        System.out.println("Hello Worldssss!");
+        int limit;
+        limit = 1000;
+        System.out.println(fizz_sum(limit));
+    }
+
+    public static int fizz_sum(int limit) {
+        int sum;
+        sum = 0;
+        for (int i = 1; i < limit; i = i + 1) {
+            if (i % 3 == 0 || i % 5 == 0){
+                sum = sum + i;
+            }
+        }
+        return sum;
     }
 }
