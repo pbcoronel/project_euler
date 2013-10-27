b6e66e35ae93315226414a7d74c4e8734876ceb7
Pedro B. Coronel <pedro.b.coronel@gmail.com>
2013-10-27 16:25:39 -0500

    old problems, new language, some new approaches

--- python/problem_001.py
+++ python/problem_001.py
@@ -1,4 +1,5 @@
 def fizzbuzz_sum(n):
-    return sum([x for x in range(n) if x%3==0 or x%5==0])
+    fizzbuzz = [x for x in range(n) if x%3==0 or x%5==0]
+    return sum(fizzbuzz)
     
 print fizzbuzz_sum(1000)
